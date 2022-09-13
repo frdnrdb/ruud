cfe0c0e43cdb1b7afc7b0625d4a4607a89e8acb9
frd nrdb <froden@gmail.com>
2022-09-13 23:26:11 +0200

    update env parser

--- src/env.js
+++ src/env.js
@@ -4,11 +4,9 @@ export default function() {
   const envFile = `${process.cwd()}/.env`;
   const env = existsSync(envFile) && readFileSync(envFile, 'utf-8');
 
-  if (!env) return;
-  
   const log = [];
-
-  env.replace(/^([^#=\n]+)=([^#\n\r]+)(?:#.+)?$/gm, (_, key, val) => {
+  
+  env && env.replace(/^([^#=\n]+)=([^#\n\r]+)(?:#.+)?$/gm, (_, key, val) => {
       if (val.trim()) {
           process.env[key] = val;
           log.push(key);
